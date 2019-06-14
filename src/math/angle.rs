5d32fda4c7942c4f7659a9589b3ca23a060b918e
Joseph Lyons <josephtlyons@gmail.com>
2019-06-14 04:34:48 -0400

    Added Turns to angle

--- src/math/angle.rs
+++ src/math/angle.rs
@@ -2,6 +2,7 @@ pub enum Units {
     Degrees,
     Radians,
     Gradians,
+    Turns,
 }
 
 pub fn convert(value: f64, from: Units, to: Units) -> f64 {
@@ -11,6 +12,7 @@ pub fn convert(value: f64, from: Units, to: Units) -> f64 {
         Units::Degrees => degrees,
         Units::Radians => degrees * (std::f64::consts::PI / 180.0),
         Units::Gradians => degrees * (10.0 / 9.0),
+        Units::Turns => degrees / 360.0,
     }
 }
 
@@ -19,6 +21,7 @@ fn to_degrees(value: f64, from: Units) -> f64 {
         Units::Degrees => value,
         Units::Radians => value * (180.0 / std::f64::consts::PI),
         Units::Gradians => value * (9.0 / 10.0),
+        Units::Turns => 360.0,
     }
 }
 
@@ -41,6 +44,15 @@ mod tests {
         assert_approx_eq!(
             convert(1.0, Units::Degrees, Units::Gradians),
             (10.0 / 9.0),
+            0.0001
+        );
+    }
+
+    #[test]
+    fn degrees_to_turns() {
+        assert_approx_eq!(
+            convert(720.0, Units::Degrees, Units::Turns),
+            2.0,
             0.0001
         );
     }
